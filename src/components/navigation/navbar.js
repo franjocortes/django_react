fd39c98ea929c315086b5d4d37189d39905dc94e
José Francisco Cortés <franjo.cortes@gmail.com>
2023-01-30 17:44:39 -0300

    navbar: config logo and button

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -1,14 +1,18 @@
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
+import mylogo from "assets/img/logo.png"
+import loading_dots from "assets/img/loading-dots.gif"
 
 function Navbar() {
    return(
-    <nav className="w-full py-10 shadow-md fixed">
+    <nav className="w-full py-4 shadow-md fixed">
         <div className="bg-white px-4 sm:px-6">
             <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                 <div className="ml-4 mt-2">
                 <img
-                src="" 
+                src={mylogo}
+                width={60}
+                height={60}
                 className="" />
                 </div>
                 <div className="ml-4 mt-2 flex-shrink-0">
@@ -20,9 +24,9 @@ function Navbar() {
                     <Link to="/contacto" className="inline-flex text-lg font-medium leading-6 text-gray-900 mx-4">Contacto</Link>
                     <button
                         type="button"
-                        className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                     >
-                        Create new job
+                        Hire us <img src={loading_dots} className="w-7 h-2 ml-4" />
                     </button>
                 </div>
             </div>

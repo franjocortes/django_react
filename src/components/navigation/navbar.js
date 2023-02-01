bfd0fff935ecc858f06f98cb37e536f877344ebd
José Francisco Cortés <franjo.cortes@gmail.com>
2023-02-01 16:37:56 -0300

    change link to navlink

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux"
-import { Link } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import mylogo from "assets/img/logo.png"
 import loading_dots from "assets/img/loading-dots.gif"
 
@@ -8,20 +8,20 @@ function Navbar() {
     <nav className="w-full py-4 top-0 fixed">
         <div className="bg-white px-4 sm:px-6">
             <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
-                <div className="ml-4 mt-2">
-                <img
-                src={mylogo}
-                width={60}
-                height={60}
-                className="" />
-                </div>
+                <Link to="/" className="ml-4 mt-2">
+                    <img
+                    src={mylogo}
+                    width={60}
+                    height={60}
+                    className="" />
+                </Link>
                 <div className="ml-4 mt-2 flex-shrink-0">
-                    <Link to="/casos" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Casos</Link>
-                    <Link to="/servicios" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Servicios</Link>
-                    <Link to="/nosotros" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Nosotros</Link>
-                    <Link to="/carreras" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Carreras</Link>
-                    <Link to="/blog" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Blog</Link>
-                    <Link to="/contacto" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Contacto</Link>
+                    <NavLink to="/casos" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Casos</NavLink>
+                    <NavLink to="/servicios" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Servicios</NavLink>
+                    <NavLink to="/nosotros" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Nosotros</NavLink>
+                    <NavLink to="/carreras" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Carreras</NavLink>
+                    <NavLink to="/blog" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Blog</NavLink>
+                    <NavLink to="/contacto" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Contacto</NavLink>
                     <button
                         type="button"
                         className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"

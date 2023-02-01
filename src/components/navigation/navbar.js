ab8d29295db47b03745997ec0f4158369673f6d5
José Francisco Cortés <franjo.cortes@gmail.com>
2023-02-01 16:55:33 -0300

    fix navbar

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -2,8 +2,13 @@ import { connect } from "react-redux"
 import { NavLink, Link } from "react-router-dom"
 import mylogo from "assets/img/logo.png"
 import loading_dots from "assets/img/loading-dots.gif"
+import { useState } from "react"
+import { DotLoader } from "react-spinners"
 
 function Navbar() {
+    
+    const [loading, setLoading] = useState(true)
+
    return(
     <nav className="w-full py-4 top-0 fixed">
         <div className="bg-white px-4 sm:px-6">
@@ -22,12 +27,14 @@ function Navbar() {
                     <NavLink to="/carreras" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Carreras</NavLink>
                     <NavLink to="/blog" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Blog</NavLink>
                     <NavLink to="/contacto" className="inline-flex text-lg font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Contacto</NavLink>
-                    <button
-                        type="button"
-                        className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+
+                    <Link
+                        to="/contacto"
+                        className="inline-flex ml-12 items-center rounded-md border border-transparent bg-orange-button px-6 py-3 text-base font-medium text-white shadow-sm transition duration-900 ease-in-out hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                     >
-                        Hire us <img src={loading_dots} className="w-7 h-2 ml-4" />
-                    </button>
+                        Hire Us
+                        <DotLoader className="ml-3 -mr-1 h-5 w-5" aria-hidden="true" loading={loading} size={20} color="#f2f2f2" />
+                    </Link>
                 </div>
             </div>
         </div>
